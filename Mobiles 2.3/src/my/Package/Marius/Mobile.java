166d059b30ea0658edb8c8ba443cef4edfb1dc82
SoulRescuer <Marius-Koeberlein@gmx.de>
2017-04-06 16:20:08 +0200

    Init

---
+++ Mobiles 2.3/src/my/Package/Marius/Mobile.java
@@ -0,0 +1,5 @@
+package my.Package.Marius;
+
+public interface Mobile {
+
+}
---
+++ Mobiles 2.3/src/my/Package/Marius/MobileTest.java
@@ -0,0 +1,10 @@
+package my.Package.Marius;
+
+public class MobileTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
